45fea9e7590dc10ebe5ad1f8e0c3ab809e909fa4
Jainam-Porwal <jainamporwal123@gmail.com>
2025-09-25 00:17:40 +0530

    folder dtructure

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directory
+mkdir -p src
+mkdir -p research
+mkdir -p data
+
+# Creating files
+touch src/__init.py
+touch src/main.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/training.ipynb
+
+
