a8e664d4afc9845429cede4d470eda7cffbe77fb
Jainam-Porwal <jainamporwal123@gmail.com>
2025-09-25 01:04:21 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+  name = "Tele Medicine Care bot",
+  author="Jainam Porwal",
+  author_email="jainamporwal123@gmail.com",
+  version="0.1.0",
+  packages=find_packages(),
+  install_requires=[]
+)
